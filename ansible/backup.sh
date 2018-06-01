75bb4f129d92fbd8f3aa4f22e658b7cd9aa41875
ChanduReddy123 <reddychandu1995@gmail.com>
2018-06-01 16:34:21 +0530

    Update backup.sh

--- ansible/backup.sh
+++ ansible/backup.sh
@@ -1,7 +1,7 @@
-if [ $(ls /home/ubuntu/backup | wc -l) -ne 0 ]
-then 
-    touch -r /home/ubuntu/backup/SpringMVCSecurityXML_v1.war    /home/ubuntu/backup/SpringMVCSecurityXML_v2.war
-    mv /home/ubuntu/backup/SpringMVCSecurityXML_v1.war    /home/ubuntu/backup/SpringMVCSecurityXML_v2.war
-fi 
-touch -r /home/ubuntu/SpringMVCSecurityXML.war /home/ubuntu/backup/SpringMVCSecurityXML_v1.war
-cp /home/ubuntu/SpringMVCSecurityXML.war /home/ubuntu/backup/SpringMVCSecurityXML_v1.war
+if [ $(ls ~/backup | wc -l) -ne 0 ]
+then
+    cp ~/backup/SpringMVCSecurityXML_v1.war    ~/backup/SpringMVCSecurityXML_v2.war
+    touch -r ~/backup/SpringMVCSecurityXML_v1.war    ~/backup/SpringMVCSecurityXML_v2.war
+fi
+cp ~/SpringMVCSecurityXML.war ~/backup/SpringMVCSecurityXML_v1.war
+touch -r ~/SpringMVCSecurityXML.war ~/backup/SpringMVCSecurityXML_v1.war
